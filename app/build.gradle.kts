902cc531735dbd9e316d421cf962149ac5ab967e
m2ebadi2021-ai <m2ebadi2021@gmail.com>
2026-09-15 12:23:59 +0330

    build: disable signing configurations
    Comment out signing configurations in build.gradle.kts to resolve build errors in local environments where keys are not present.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,21 +23,21 @@ android {
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
 
-  signingConfigs {
-    create("release") {
-      val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
-      storeFile = file(keystorePath)
-      storePassword = System.getenv("STORE_PASSWORD")
-      keyAlias = "upload"
-      keyPassword = System.getenv("KEY_PASSWORD")
-    }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
-  }
+  // signingConfigs {
+  //   create("release") {
+  //     val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+  //     storeFile = file(keystorePath)
+  //     storePassword = System.getenv("STORE_PASSWORD")
+  //     keyAlias = "upload"
+  //     keyPassword = System.getenv("KEY_PASSWORD")
+  //   }
+  //   create("debugConfig") {
+  //     storeFile = file("${rootDir}/debug.keystore")
+  //     storePassword = "android"
+  //     keyAlias = "androiddebugkey"
+  //     keyPassword = "android"
+  //   }
+  // }
 
   buildTypes {
     release {
@@ -46,7 +46,9 @@ android {
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
-    debug { signingConfig = signingConfigs.getByName("debugConfig") }
+    debug {
+      //  signingConfig = signingConfigs.getByName("debugConfig")
+        }
   }
   compileOptions {
     sourceCompatibility = JavaVersion.VERSION_11
